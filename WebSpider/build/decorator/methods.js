e6445f2ec663126f8cb87fd745c3e8a18af14efb
windome1109 <wuzengyan1314@163.com>
2020-08-25 18:31:30 +0800

    TypeScript 爬虫项目——后端

---
+++ WebSpider/build/decorator/methods.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Methods = void 0;
+var Methods;
+(function (Methods) {
+    Methods["GET"] = "get";
+    Methods["POST"] = "post";
+})(Methods = exports.Methods || (exports.Methods = {}));
